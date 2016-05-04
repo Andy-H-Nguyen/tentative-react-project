410dc70105f630a35fb69488e8c7c7d62677d51c
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-05-04 20:26:30 +0000

    add props

--- public/app.jsx
+++ public/app.jsx
@@ -1,14 +1,23 @@
 var Greeter = React.createClass({
+  getDefaultProps: function () {
+    return {
+      name: 'React' ,
+      message: 'This is a component'
+    }
+  },
   render: function () {
+    var name = this.props.name;
+    var message = this.props.message
     return (
         <div>
-          <h1>Hello React!</h1>,
+          <h1>Hello {name}!</h1>
+          <p>{message}</p>
         </div>
       ); 
   }
 });
 
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name="Andy" message="This is a custom msg"/>,
   document.getElementById('app')
 );
