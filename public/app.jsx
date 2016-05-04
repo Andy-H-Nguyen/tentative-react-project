e299b7c440f9e9575d7ed98d094ef66af412c061
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-05-04 22:09:15 +0000

    add component state

--- public/app.jsx
+++ public/app.jsx
@@ -5,19 +5,42 @@ var Greeter = React.createClass({
       message: 'This is a component'
     }
   },
+  getInitialState: function () {
+    return {
+      name: this.props.name 
+    }
+  },
+  onButtonClick: function (e) {
+    e.preventDefault();
+    
+    var nameRef = this.ref.name;
+    var name = nameRef.value;
+    nameRef.value = '';
+    
+    if (typeof name === 'string' && name.length > 0) {
+      this.setState({
+        name: name
+      });
+    }
+  },
   render: function () {
-    var name = this.props.name;
+    var name = this.state.name;
     var message = this.props.message
     return (
         <div>
           <h1>Hello {name}!</h1>
           <p>{message}</p>
+          
+          <form onSubmit={this.onButtonClick}>
+            <input type="text" ref="name"/>
+            <button>Set Name</button>
+          </form>
         </div>
       ); 
   }
 });
 
 ReactDOM.render(
-  <Greeter name="Andy" message="This is a custom msg"/>,
+  <Greeter name="Andy"/>,
   document.getElementById('app')
 );
