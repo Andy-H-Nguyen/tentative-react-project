c7252ebaceb7e93d0e86db6a81302f5b7a07ee2f
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-05-05 20:07:16 +0000

    setup webpack

--- public/app.jsx
+++ public/app.jsx
@@ -1,5 +1,5 @@
-/* global React */
-/* global ReactDOM */
+var React = require('react');
+var ReactDOM = require('react-dom');
 
 // Messages
 var GreeterMessage = React.createClass({
---
+++ public/components/Greeter.js
@@ -0,0 +1,5 @@
+function greeter() {
+    document.write("From greeter fn");   
+}
+
+module.exports = greeter;
