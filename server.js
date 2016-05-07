327d233023e7a0faec7c24ca4f67bae3adfb1efd
Andy Nguyen <andy.h.nguyen1996@gmail.com>
2016-05-07 02:42:01 +0000

    prep boilerplate

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
-var PORT = process.env.PORT;
+
 // Create our app
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
